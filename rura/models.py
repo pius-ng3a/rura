1fc268c5098a17eb1abcb5114ab780c147cabf19
pius-ng3a <pius2016@gmail.com>
2020-03-03 14:55:36 +0200

    Creating Templates

--- rura/models.py
+++ rura/models.py
@@ -6,7 +6,7 @@ class ICT(models.Model):
     date_paid = models.DateTimeField(auto_now=False) #necessary to have this cuz recording could be made at a later date than actual payment month
     postpaid = models.CharField(max_length=30)
     prepaid = models.CharField(max_length=30)
-    created_on = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now=True)
     mobile_subscribers = models.CharField(max_length=40)
     fixed_subscribers = models.CharField(max_length=40)
     class Meta:
@@ -17,7 +17,7 @@ class InternationalInternetBandwidth(models.Model):
     company_name = models.CharField(max_length=30)
     up_link = models.CharField(max_length=20)
     down_link = models.CharField(max_length=20)
-    created_on = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now=True)
     class Meta:
         ordering =['created_on']
     def __unicode__(self):
@@ -28,7 +28,7 @@ class VoiceTraffic(models.Model):
     call_count = models.CharField(max_length=40)
     out_going = models.BooleanField(default=True)
     average_minute_per_call = models.CharField(max_length=5)
-    created_on = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now=True)
     class Meta:
         ordering =['created_on']
     def __unicode__(self):
--- rura/views.py
+++ rura/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from rura.models import ICT,InternationalInternetBandwidth,VoiceTraffic
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = "home.html"
+    def getICTMonthlyReport(self,request):
+        ict = ICT.objects.all()
+        args ={'ict':ict}
+        return render(request,self.template_name,args)
+
 def homePageView(request):
     return HttpResponse("With data")
