d2a518cc8f7c0d3d37fde7533855929527340e16
pius-ng3a <pius2016@gmail.com>
2020-04-18 12:30:35 +0200

    new graphs

--- rura/urls.py
+++ rura/urls.py
@@ -1,8 +1,14 @@
 from django.urls import  path
 from . import  views
+from rura.views import getBarChatData,getICT
+#from django.contrib.auth.views import login
+#from rura.views import home, contact
+
 urlpatterns = [
     #path('',views.HomePageView.as_view(),name='home'),
     path('',views.homePageView,name="home"),
+    path('get/patient/bar/data',getBarChatData,name="chart_data"),
+    path('get/prepaid/ict/data',getICT,name="ict_prepaid")
     #path('stats/monthly', name='monthlyStatistics'),
     #path('stats/quarterly',name='quarterlyStatistics')
 ]
--- rura/views.py
+++ rura/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.http import JsonResponse
 from rura.models import ICT,InternationalInternetBandwidth,VoiceTraffic
+from datetime import datetime
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = "home.html"
@@ -11,10 +13,36 @@ class HomePageView(TemplateView):
         vt =VoiceTraffic.objects.all()
         args ={'ict':ict,'vt':vt}
         return render(self.template_name,args)
-
+    
 def homePageView(request):
     template_name = "home.html"
     icts = ICT.objects.all()
     vt =VoiceTraffic.objects.all()
     args ={'icts':icts,'vts':vt}
+
     return render(request, template_name,args)
+
+def getBarChatData(request):
+    template_name = "home.html"
+    ict = ICT.objects.all()
+    vt =VoiceTraffic.objects.all()
+    iternationalIB = InternationalInternetBandwidth.objects.all()
+    dt = {
+        "data":[len(ict),len(vt),len(iternationalIB)],
+        "labels":["ICT ","voice Traffic","Intl Bandwidth"]
+    }
+    return JsonResponse(dt)
+
+def getICT(request):
+    icts = ICT.objects.all()
+    vals= []
+    labs =[]
+    for ict in icts: 
+        vals.append(ict.prepaid)
+        labs.append(datetime.date(ict.date_paid))
+       
+    dt={
+        "data":vals,
+        "labels":labs
+    }
+    return JsonResponse(dt)
